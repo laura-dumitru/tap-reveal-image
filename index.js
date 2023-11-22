4ca170daf0b5152d7acea9054747de3d0d09744c
Laura Dumitru <lauradumitru88@icloud.com>
2023-11-22 11:55:45 +0000

    Link Js file

--- index.js
+++ index.js
@@ -24,153 +24,156 @@ let x, y;
 let image;
 let lastTouchedCta;
 
-const imagesGroup = screen.find("Images");
-const hotspotsGroup = screen.find("Hotspots");
-const hotspotsArray = hotspotsGroup.content.objects;
-const imagesArray = imagesGroup.content.objects;
-
-const ctaGroup = screen.find("ctaGroup");
-const ctaArray = ctaGroup.content.objects;
-const arrow = screen.find("nextArrow") || screen.find("previousArrow");
+const imagesGroup = document.querySelectorAll(".grid img");
+console.log(imagesGroup);
+const ctaGroup = document.querySelector(".cta");
 
 function init() {
   // Loop over imagesGroup and hide each one with opacity 0
   function hideImages() {
-    imagesArray.forEach((image) => image.setOpacity(0));
+    imagesGroup.forEach((image) => (image.style.opacity = 0));
   }
+  hideImages();
+}
 
-  // Loop over all cta layers and hide when the ad loads
-  function hideCtas() {
-    ctaArray.forEach((cta) => cta.hideAction(ctx, {}, noop));
-  }
-  hideCtas();
+function followFinger(event) {
+  event.preventDefault();
 
-  function showLastCta() {
-    if (lastTouchedCta) {
-      lastTouchedCta.showAction(ctx, {}, noop); // show the last touched CTA on touchend
-    }
-  }
-
-  function followFinger(event) {
-    event.preventDefault();
-
-    if (mobile) {
-      // For mobile, use touch event coordinates
-      x = event.touches[0].clientX;
-      y = event.touches[0].clientY;
-    } else {
-      // For desktop, use mouse event coordinates
-      x = event.clientX;
-      y = event.clientY;
-    }
-
-    // Position the image to the centre
-    function positionImage(image, x, y) {
-      const xOffset = x - options.offsetX;
-      const yOffset = y - options.offsetY;
-      // Calculate the position relative to the hotspot
-      image.node.style.top = `calc(${yOffset}px - ${image.node.style.height} / 2)`;
-      image.node.style.left = `calc(${xOffset}px - ${image.node.style.width}  / 2)`;
-    }
-
-    if (moving) {
-      // if the user is touching or hovering the screen
-      hotspotsArray
-        .filter((hotspot) => hotspot instanceof Hotspot) // filtering out any layers that are not hotspots
-        .forEach((hotspot) => {
-          const hotspotsValues = options.hotspots[hotspot.name];
-
-          image = screen.find(hotspotsValues[0]);
-          image.node.style.transition = `opacity ${options.transition}`;
-
-          // Calculating whether the finger is inside the boundaries of the hotspot
-          const {
-            left,
-            right,
-            top,
-            bottom,
-          } = hotspot.node.getBoundingClientRect();
-
-          if (x >= left && x <= right && y >= top && y <= bottom) {
-            // if I am in the boundaries of the box set the opacity of the image to 1
-            image.setOpacity(1);
-            lastTouchedCta = screen.find(hotspotsValues[1]);
-            mediator(`User viewed image ${hotspotsValues[0]}`);
-          } else if (options.hideImage) {
-            //console.log("outside");
-            image.setOpacity(0); // when finger no longer in the box hide image
-          }
-
-          if (options.stickyImage) {
-            positionImage(image, x, y); //options.transition
-          }
-        });
-    }
-  }
-
-  // add touchstart and touchend event listeners for mobile
   if (mobile) {
-    document.addEventListener("touchmove", followFinger);
-
-    hotspotsGroup.node.addEventListener("touchstart", (event) => {
-      event.preventDefault();
-      moving = true;
-      followFinger(event);
-    });
-
-    hotspotsGroup.node.addEventListener("touchend", (event) => {
-      event.preventDefault();
-      moving = false;
-
-      if (options.stickyImage || (options.staticImage && options.hideImage)) {
-        imagesArray.forEach((image) => {
-          image.node.style.transition = `opacity ${options.transition}`;
-          image.setOpacity(0); // when finger no longer in the box hide image
-          // Keep the image at its original size (no scaling)
-          image.node.style.transform = "scale(1)";
-        });
-        hideImages();
-        imagesArray.forEach((image) => {
-          if (options.stickyImage) {
-            image.node.style.transition = ""; // Remove CSS transition (fade)
-            // TOOD this hides the 'snapping' back
-          }
-          image.setOpacity(0); // when finger no longer in the box hide image
-        });
-        image = null; // reset
-      }
-      hideCtas();
-      showLastCta();
-    });
-
-    // add mousemove (hover) event listener for desktop
+    // For mobile, use touch event coordinates
+    x = event.touches[0].clientX;
+    y = event.touches[0].clientY;
   } else {
-    document.addEventListener("mousemove", followFinger);
+    // For desktop, use mouse event coordinates
+    x = event.clientX;
+    y = event.clientY;
+  }
 
-    hotspotsGroup.node.addEventListener("mousemove", (event) => {
-      //console.log("mousemove")
-      moving = true;
-      followFinger(event);
-      hideCtas();
-      showLastCta();
-    });
-
-    if (arrow) {
-      arrow.node.addEventListener("click", (event) => {
-        const page = unit.find(options.goToPage);
-        unit.goToScreenAction(
-          ctx,
-          {
-            screen: page,
-            animation: {
-              animation: options.animationType,
-              duration: options.fadeDuration,
-              direction: options.direction,
-            },
-          },
-          noop
-        );
-      });
-    }
+  // Position the image to the centre
+  function positionImage(image, x, y) {
+    const xOffset = x - options.offsetX;
+    const yOffset = y - options.offsetY;
+    // Calculate the position relative to the hotspot
+    image.style.top = `calc(${yOffset}px - ${image.style.height} / 2)`;
+    image.style.left = `calc(${xOffset}px - ${image.style.width}  / 2)`;
   }
 }
+
+if (mobile) {
+  document.addEventListener("touchmove", followFinger);
+
+  imagesGroup.forEach((image) => {
+    image.addEventListener("touchstart", (event) => {
+      console.log("Touch start");
+      moving = true;
+      followFinger(event);
+    });
+
+    image.addEventListener("touchend", (event) => {
+      console.log("Touch end");
+
+      moving = false;
+    });
+  });
+}
+init();
+
+/*
+
+
+function init() {
+
+
+
+		if (moving) {
+			// if the user is touching or hovering the screen
+			hotspotsArray
+				.filter((hotspot) => hotspot instanceof Hotspot) // filtering out any layers that are not hotspots
+				.forEach((hotspot) => {
+					const hotspotsValues = options.hotspots[hotspot.name];
+
+					image = screen.find(hotspotsValues[0]);
+					image.node.style.transition = `opacity ${options.transition}`;
+
+					// Calculating whether the finger is inside the boundaries of the hotspot
+					const { left, right, top, bottom } = hotspot.node.getBoundingClientRect();
+
+					if (x >= left && x <= right && y >= top && y <= bottom) {
+						if (options.video) {
+							video.pauseAction(ctx, {}, noop);
+							mediator(`Video paused`);
+						}
+						// if I am in the boundaries of the box set the opacity of the image to 1
+						image.setOpacity(1);
+						lastTouchedCta = screen.find(hotspotsValues[1]);
+						mediator(`User viewed image ${hotspotsValues[0]}`);
+					} else if (options.hideImage) {
+						//console.log("outside");
+						image.setOpacity(0); // when finger no longer in the box hide image
+					}
+
+					if (options.stickyImage) {
+					    positionImage(image, x, y); //options.transition
+					}
+				});
+		}
+	}
+
+	// add touchstart and touchend event listeners for mobile
+	if (mobile) {
+		document.addEventListener('touchmove', followFinger);
+
+		hotspotsGroup.node.addEventListener('touchstart', (event) => {
+			event.preventDefault();
+			moving = true;
+			followFinger(event);
+		});
+
+		hotspotsGroup.node.addEventListener('touchend', (event) => {
+			event.preventDefault();
+			moving = false;
+
+			if (options.video) {
+				video.playAction(ctx, {}, noop);
+				mediator(`Video playing`);
+			}
+
+			if (options.stickyImage || (options.staticImage && options.hideImage)) {
+				imagesArray.forEach((image) => {
+					image.node.style.transition = `opacity ${options.transition}`;
+					image.setOpacity(0); // when finger no longer in the box hide image
+					// Keep the image at its original size (no scaling)
+					image.node.style.transform = 'scale(1)';
+				});
+				hideImages();
+				imagesArray.forEach((image) => {
+					if (options.stickyImage) {
+						image.node.style.transition = ''; // Remove CSS transition (fade)
+						// TOOD this hides the 'snapping' back
+					}
+					image.setOpacity(0); // when finger no longer in the box hide image
+				});
+				image = null; // reset
+			}
+			hideCtas();
+			showLastCta();
+		});
+
+		// add mousemove (hover) event listener for desktop
+	} else {
+		document.addEventListener('mousemove', followFinger);
+
+		hotspotsGroup.node.addEventListener('mousemove', (event) => {
+			//console.log("mousemove")
+			moving = true;
+			followFinger(event);
+			hideCtas();
+			showLastCta();
+		});
+		// adding mouseout even listener and looping over the contents of the hotspot group to play the video
+
+	}
+
+}
+
+*/
