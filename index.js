000c4a197d4b176233370d60748bdcc22a9b2e40
Laura Dumitru <lauradumitru88@icloud.com>
2024-01-09 15:55:53 +0000

    Tidy code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let options = {
   stickyImage: false, // follows the user's finger
-  staticImage: true, //stops on the screen when the user lets go, doesn't follow finger.
+  staticImage: true, // stops on the screen when the user lets go, doesn't follow finger.
   hideImage: true, // shows the image after the finger leaves the hotspot. If sticky, this option should stay true.
-  transition: "0.5s", //duration in seconds that it takes for the change from one image to another.
+  transition: "0.5s", // duration in seconds that it takes for the change from one image to another.
   offsetX: 0, // horizontal offset - value in pixels.It determines how far to the left or right of your thumb the image will appear.
   offsetY: 0, // vertical offset - value in pixels. It determines how far above or below your thumb the image will appear.
   products: ["google.com", "instagram.com", "facebook.com", "youtube.com"],
@@ -11,25 +11,20 @@ let options = {
 const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
 const mobile = regex.test(navigator.userAgent);
 
+const hotspots = Array.from(document.querySelectorAll(".grid .hotspot"));
+const images = Array.from(document.querySelectorAll(".grid img"));
 const cta = document.querySelector(".cta a");
+
+// let lastTouchedCta = options.products[0];
 let moving = false;
 let x, y;
 let image;
-let lastTouchedCta;
-
-const hotspots = Array.from(document.querySelectorAll(".grid .hotspot"));
-const images = Array.from(document.querySelectorAll(".grid img"));
-const buttons = document.querySelectorAll("button");
 
 // Loop over images and hide each one with opacity 0
 function hideImages() {
   images.forEach((image) => (image.style.opacity = 0));
 }
 hideImages();
-
-function showImages() {
-  images.forEach((image) => (image.style.opacity = 1));
-}
 
 function followFinger(event) {
   event.preventDefault();
@@ -46,7 +41,6 @@ function followFinger(event) {
       const offsetX = options.offsetX;
       const offsetY = options.offsetY;
       // Calculate the translation values to keep the image centered in the hotspot
-
       const xOffset = x - hotspotRect.left - offsetX;
       const yOffset = y - hotspotRect.top - offsetY;
       image.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
@@ -55,25 +49,15 @@ function followFinger(event) {
 
   if (moving) {
     // Iterate over each hotspot to check if the finger is inside
-    //const hotspot = hotspots.find() {
+    // const hotspot = hotspots.find() {
     hotspots.forEach(function (hotspot, index) {
       //if inside rect true else false
       const rect = hotspot.getBoundingClientRect();
       const image = images[index];
       image.style.transition = `opacity ${options.transition}`;
-      if (
-        x >= rect.left &&
-        x <= rect.right &&
-        y >= rect.top &&
-        y <= rect.bottom
-      ) {
-        if (
-          options.staticImage ||
-          (options.stickyImage && !options.hideImage)
-        ) {
-          image.style.opacity = 1;
-        }
-        //console.log(`Finger is inside hotspot ${index}`);
+
+      if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+        image.style.opacity = 1;
       } else {
         if (options.hideImage) {
           image.style.opacity = 0;
@@ -108,7 +92,6 @@ if (mobile) {
 
   images.forEach((image, index) => {
     image.addEventListener("mousemove", (event) => {
-      //console.log("mousemove");
       moving = true;
       cta.href = `https://${options.products[index]}`;
       followFinger(event);
